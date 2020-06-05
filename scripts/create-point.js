3a6141c4a696a0f87271087f79448fe6ef3af8c4
Rafael <cryptogranacap@gmail.com>
2020-06-05 13:30:36 -0300

    bug cidades e estados no formulário solucionado

--- scripts/create-point.js
+++ scripts/create-point.js
@@ -26,6 +26,9 @@ function getCities(event){
 
     const url = `https://servicodados.ibge.gov.br/api/v1/localidades/estados/${ufValue}/municipios`
 
+    citySelect.innerHTML = "<option value> Selecione a Cidade</option>"
+    citySelect.disabled = true
+
     fetch(url)
         .then((res) => { return res.json()})
         .then( cities => {
