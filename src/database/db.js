f5c11391a288ad2e5e3affd4933686e292d18e8a
chxpz <cryptogranacap@gmail.com>
2020-06-07 08:44:48 -0300

    criação de tabela e estrutura database

--- src/database/db.js
+++ src/database/db.js
@@ -11,15 +11,69 @@ db.serialize(() => {
     //criar uma tabela
     db.run(`
         CREATE TABLE IF NOT EXISTS places (
-            id INTEGER PRIMARY KEY AUTOINCREMENT
+            id INTEGER PRIMARY KEY AUTOINCREMENT,
+            image TEXT,
+            name TEXT,
+            address TEXT,
+            address2 TEXT,
+            state TEXT,
+            city TEXT,
+            items TEXT
         );
     `)
 
-
-
     //inserir dados
+    const query = `
+    INSERT INTO places (
+        image,
+        name,
+        address,
+        address2,
+        state,
+        city,
+        items
+    ) VALUES (?,?,?,?,?,?,?);
+`
+    
+    const values = [
+        "https://images.unsplash.com/photo-1567393528677-d6adae7d4a0a?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1350&q=80",
+        "Colectoria",
+        "Guilherme Gembala, Jd America",
+        "Numero 260",
+        "Santa Catarina",
+        "Rio do Sul",
+        "Resíduos Eletrônicos e Lâmpadas"
+        
+    ]
+
+    function afterInserData(err){
+        if(err) {
+            return console.log(err)
+        }
+        console.log("Cadastrado ok")
+        console.log(this)
+    }
+    
+    db.run (query, values, afterInserData)
+
 
     //consultar dados
+    db.all(`
+        SELECT * FROM places`, function(err,rows){
+            if(err){
+                return console.log(err)
+            }
+            console.log("Esses são os registros: ")
+            console.log(rows)
+        })
 
     //deletar um dado
+        /*db.run(`
+            DELETE FROM places WHERE id = ?`, [1], function (err){
+                if(err){
+                    return console.log(err)
+                }
+                console.log("Registro deletado")
+            })*/
+    
 })
